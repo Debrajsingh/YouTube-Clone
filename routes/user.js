e376383974d05ad01bbb2ae48f4f8ab4cda673cf
Debrajsingh <debrajdeb201@gmail.com>
2025-08-26 20:29:23 +0530

    added password verification

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt')
 const cloudinary = require('cloudinary').v2;
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
@@ -55,7 +56,7 @@ Router.post('/login',async(req,res)=>{
     
         try 
         {
-            console.log(req.body)
+            //console.log(req.body)
             const users = await User.find({email:req.body.email})
             console.log(users)
             if(users.length==0)
@@ -65,7 +66,46 @@ Router.post('/login',async(req,res)=>{
     
                 })
             }
-        } 
+            
+           const isValid = await bcrypt.compare(req.body.password,users[0].password)
+           console.log(isValid)
+           if(!isValid)
+           {
+            return res.status(500).json({
+                error:'Incorrect Password'
+
+            })
+           }
+              const token = jwt.sign({
+                _id:users[0]._id,
+                channelName:users[0].channelName,
+                email:users[0].email,
+                phone:users[0].phone,
+                logoId:users[0].logoId,
+                
+              },
+              'Liza loves Debraj',
+              {
+                expiresIn:'365d'
+
+              }
+              
+              )
+                res.status(200).json({
+                    _id:users[0]._id,
+                 channelName:users[0].channelName,
+                email:users[0].email,
+                phone:users[0].phone,
+                logoId:users[0].logoId,
+                logoUrl:users[0].logoUrl,
+                subscribers:users[0].subscribers,
+                subscribedChannels:users[0].subscribedChannels,
+                    message:'Login Successful',
+                    token:token
+                })
+              
+           
+        }
         catch (err) 
         {
             console.log(err)
