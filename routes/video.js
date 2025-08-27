79df3e08837b329b3045184d629f1d185787f90f
Debrajsingh <debrajdeb201@gmail.com>
2025-08-27 17:01:42 +0530

    api for updating video details completed

--- routes/video.js
+++ routes/video.js
@@ -5,6 +5,8 @@ const jwt = require('jsonwebtoken');
 const cloudinary = require('cloudinary').v2;
 const Video = require('../models/Video');
 const mongoose = require('mongoose');
+const e = require('express');
+
 
 
 cloudinary.config({
@@ -34,7 +36,7 @@ Router.post('/upload',checkAuth,async(req,res) => {
             videoUrl: uploadedVideo.secure_url,
             videoId: uploadedVideo.public_id,
             thumbnailUrl: uploadedThumbnail.secure_url,
-            thumbnailId: uploadedThumbnail.secure_url,
+            thumbnailId: uploadedThumbnail.public_id,
             category: req.body.category,
             tags: req.body.tags.split(','),
         });
@@ -53,5 +55,70 @@ Router.post('/upload',checkAuth,async(req,res) => {
     
 });
 
+//update video detail
+Router.put('/:videoId',checkAuth,async(req,res)=>{
+    try{
+        const verifiedUser = await jwt.verify(req.headers.authorization.split(' ')[1], 'Liza loves Debraj'); 
+        const video = await Video.findById(req.params.videoId)
+        console.log(video);
+
+        if(video.user_id === verifiedUser._id)
+        {
+            //update video details
+            if(req.files)
+            {
+                //update thumbnail and text data
+                await cloudinary.uploader.destroy(video.thumbnailId);
+                const updatedThumbnail = await cloudinary.uploader.upload(req.files.thumbnail.tempFilePath);
+                const updatedData = {
+                    title: req.body.title,
+                    description: req.body.description,
+                    category: req.body.category,
+                    tags: req.body.tags.split(','),
+                    thumbnailUrl: updatedThumbnail.secure_url,
+                    thumbnailId: updatedThumbnail.public_id,
+                }
+
+                const updatedVideoDetail = await Video.findByIdAndUpdate(req.params.videoId,updatedData,{ new: true })
+                res.status(200).json({
+                    updatedVideo: updatedVideoDetail
+                })
+
+               
+            }
+            else
+            {
+                const updatedData = {
+                    title: req.body.title,
+                    description: req.body.description,
+                    category: req.body.category,
+                    tags: req.body.tags.split(','),
+                    
+                }
+
+                const updatedVideoDetail = await Video.findByIdAndUpdate(req.params.videoId,updatedData,{ new: true })
+                res.status(200).json({
+                    updatedVideo: updatedVideoDetail
+                })
+                
+
+
+            }
+            
+            
+        }
+        else
+        {
+            return res.status(500).json({
+                error:'You are not authorized to update this video'
+            });
+        }
+    }
+    catch(err)
+    {
+        console.log(err);
+        res.status(500).json({error:err});
+    }
+});
 
 module.exports = Router;
