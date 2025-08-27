b0104b7d611fb8bacc36f1277c8972c6d8d4652b
Debrajsingh <debrajdeb201@gmail.com>
2025-08-27 14:23:17 +0530

    saving data into database and upload video api completed

--- routes/video.js
+++ routes/video.js
@@ -3,6 +3,8 @@ const Router = express.Router();
 const checkAuth = require('../middleware/checkAuth');
 const jwt = require('jsonwebtoken');
 const cloudinary = require('cloudinary').v2;
+const Video = require('../models/Video');
+const mongoose = require('mongoose');
 
 
 cloudinary.config({
@@ -24,8 +26,23 @@ Router.post('/upload',checkAuth,async(req,res) => {
         });
         const uploadedThumbnail = await cloudinary.uploader.upload(req.files.thumbnail.tempFilePath);
         
-        
+        const newVideo = new Video({
+            _id: new mongoose.Types.ObjectId(),
+            title: req.body.title,
+            description: req.body.description,
+            user_id: user._id,
+            videoUrl: uploadedVideo.secure_url,
+            videoId: uploadedVideo.public_id,
+            thumbnailUrl: uploadedThumbnail.secure_url,
+            thumbnailId: uploadedThumbnail.secure_url,
+            category: req.body.category,
+            tags: req.body.tags.split(','),
+        });
 
+        const newUploadedVideoData = await newVideo.save();
+        res.status(200).json({
+            newVideo: newUploadedVideoData
+        });
         
     }
     catch(err)
