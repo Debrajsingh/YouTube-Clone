a04f2e6f5a2ccadbda9c0a300257231f2ddacf73
Debrajsingh <debrajdeb201@gmail.com>
2025-08-28 02:25:26 +0530

    subscribe channel part completed

--- routes/user.js
+++ routes/user.js
@@ -1,118 +1,135 @@
-const express = require('express');
+const express = require("express");
 const Router = express.Router();
-const User = require('../models/User');
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt')
-const cloudinary = require('cloudinary').v2;
-require('dotenv').config()
-const jwt = require('jsonwebtoken');
+const User = require("../models/User");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const cloudinary = require("cloudinary").v2;
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const checkAuth = require("../middleware/checkAuth");
 
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET,
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
 });
 
-Router.post('/signup',async(req,res)=>{
-    try
-    {
-       const users = await User.find({email:req.body.email})
-       if(users.length>0)
-       {
-        return res.status(500).json({
-            error:'User with this email already exists'
-
-        })
-       }
-
-
-       const hashCode = await bcrypt.hash(req.body.password,10,)
-       const uploadedImage = await cloudinary.uploader.upload(req.files.logo.tempFilePath,)
-       
-       const newUser = new User({
-        _id: new mongoose.Types.ObjectId(),
-        channelName: req.body.channelName,
-        email: req.body.email,
-        phone: req.body.phone,
-        password: hashCode,
-        logoUrl: uploadedImage.secure_url,
-        logoId: uploadedImage.public_id,
-       })
-
-        const user = await newUser.save()
-        res.status(200).json({
-            newUser:user
-        })
-
+Router.post("/signup", async (req, res) => {
+  try {
+    const users = await User.find({ email: req.body.email });
+    if (users.length > 0) {
+      return res.status(500).json({
+        error: "User with this email already exists",
+      });
     }
-    catch(err)
-    {
-        console.log(err)
-        res.status(500).json({error:err})        
+
+    const hashCode = await bcrypt.hash(req.body.password, 10);
+    const uploadedImage = await cloudinary.uploader.upload(
+      req.files.logo.tempFilePath
+    );
+
+    const newUser = new User({
+      _id: new mongoose.Types.ObjectId(),
+      channelName: req.body.channelName,
+      email: req.body.email,
+      phone: req.body.phone,
+      password: hashCode,
+      logoUrl: uploadedImage.secure_url,
+      logoId: uploadedImage.public_id,
+    });
+
+    const user = await newUser.save();
+    res.status(200).json({
+      newUser: user,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err });
+  }
+});
+
+Router.post("/login", async (req, res) => {
+  try {
+    //console.log(req.body)
+    const users = await User.find({ email: req.body.email });
+    //console.log(users)
+    if (users.length == 0) {
+      return res.status(500).json({
+        error: "User with this email does not exists",
+      });
     }
-})
 
-Router.post('/login',async(req,res)=>{
-    
-        try 
-        {
-            //console.log(req.body)
-            const users = await User.find({email:req.body.email})
-            //console.log(users)
-            if(users.length==0)
-            {
-                return res.status(500).json({
-                    error:'User with this email does not exists'
-    
-                })
-            }
-            
-           const isValid = await bcrypt.compare(req.body.password,users[0].password)
-           //console.log(isValid)
-           if(!isValid)
-           {
-            return res.status(500).json({
-                error:'Incorrect Password'
+    const isValid = await bcrypt.compare(req.body.password, users[0].password);
+    //console.log(isValid)
+    if (!isValid) {
+      return res.status(500).json({
+        error: "Incorrect Password",
+      });
+    }
+    const token = jwt.sign(
+      {
+        _id: users[0]._id,
+        channelName: users[0].channelName,
+        email: users[0].email,
+        phone: users[0].phone,
+        logoId: users[0].logoId,
+      },
+      "Liza loves Debraj",
+      {
+        expiresIn: "365d",
+      }
+    );
+    res.status(200).json({
+      _id: users[0]._id,
+      channelName: users[0].channelName,
+      email: users[0].email,
+      phone: users[0].phone,
+      logoId: users[0].logoId,
+      logoUrl: users[0].logoUrl,
+      subscribers: users[0].subscribers,
+      subscribedChannels: users[0].subscribedChannels,
+      message: "Login Successful",
+      token: token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      error: "Something went wrong",
+    });
+  }
+});
 
-            })
-           }
-              const token = jwt.sign({
-                _id:users[0]._id,
-                channelName:users[0].channelName,
-                email:users[0].email,
-                phone:users[0].phone,
-                logoId:users[0].logoId,
-                
-              },
-              'Liza loves Debraj',
-              {
-                expiresIn:'365d'
+//subscribe api
 
-              }
-              
-              )
-                res.status(200).json({
-                    _id:users[0]._id,
-                 channelName:users[0].channelName,
-                email:users[0].email,
-                phone:users[0].phone,
-                logoId:users[0].logoId,
-                logoUrl:users[0].logoUrl,
-                subscribers:users[0].subscribers,
-                subscribedChannels:users[0].subscribedChannels,
-                    message:'Login Successful',
-                    token:token
-                })
-              
-           
-        }
-        catch (err) 
-        {
-            console.log(err)
-            res.status(500).json({
-                error:'Something went wrong'
-            })
-        }
-})
+Router.put("/subscribe/:userBId", checkAuth, async (req, res) => {
+  try {
+    const userA = await jwt.verify(
+      req.headers.authorization.split(" ")[1],
+      "Liza loves Debraj"
+    );
+    //userA is the one who is subscribing
+    //userB is the one who is being subscribed to
+    console.log(userA);
+    const userB = await User.findById(req.params.userBId);
+    console.log(userB);
+    if (userB.subscribedBy.includes(userA._id)) {
+      return res.status(500).json({
+        error: "You have already subscribed to this channel",
+      });
+    }
+    userB.subscribers += 1;
+    userB.subscribedBy.push(userA._id);
+    await userB.save();
+    const userAFullInformation = await User.findById(userA._id);
+    userAFullInformation.subscribedChannels.push(userB._id);
+    await userAFullInformation.save();
+    res.status(200).json({
+      message: "Subscription successful",
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err });
+  }
+});
 
-module.exports = Router
+module.exports = Router;
