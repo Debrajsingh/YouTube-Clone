4c916aa65ea0b1ddfa48f719b119cc066dfd971c
Debrajsingh <debrajdeb201@gmail.com>
2025-08-28 16:32:07 +0530

    comment likes and dislikes api

--- models/Comment.js
+++ models/Comment.js
@@ -5,6 +5,11 @@ const commentSchema = new mongoose.Schema({
     userId:{type:mongoose.Schema.Types.ObjectId, required:true,ref:'User'},
     videoId:{type:String, required:true},
     commentText:{type:String, required:true},
+    likes:{type:Number, default:0},
+    dislike:{type:Number, default:0},
+    likedBy:[{type:mongoose.Schema.Types.ObjectId, ref:'User'}],
+    dislikedBy:[{type:mongoose.Schema.Types.ObjectId, ref:'User'}],
+    
     
     
 },{timestamps:true});
--- routes/comment.js
+++ routes/comment.js
@@ -45,5 +45,74 @@ Router.get('/:videoId', async(req,res)=>{
     }
 })
 
+//like api
+Router.put('/like/:commentId',checkAuth,async(req,res)=>{
+    try
+    {
+        const verifiedUser = await jwt.verify(req.headers.authorization.split(' ')[1], 'Liza loves Debraj'); 
+        console.log(verifiedUser);
+        const comment = await Comment.findById(req.params.commentId)
+        console.log(comment);
+        if(comment.likedBy.includes(verifiedUser._id))
+        {
+            return res.status(500).json({
+                error:'You have already liked this comment'
+            });
+        }
+
+        if(comment.dislikedBy.includes(verifiedUser._id))
+        {
+            comment.dislike -= 1;
+            comment.dislikedBy = comment.dislikedBy.filter(userId => userId.toString() !== verifiedUser._id);
+        }
+        comment.likes += 1;
+        comment.likedBy.push(verifiedUser._id);
+        await comment.save();
+        res.status(200).json({
+            message:'Comment liked',
+            
+        });
+    }
+    catch(err)
+    {
+        console.log(err);
+        res.status(500).json({error:err});
+    }
+});
+//dislike api
+Router.put('/dislike/:commentId',checkAuth,async(req,res)=>{
+    try
+    {
+        const verifiedUser = await jwt.verify(req.headers.authorization.split(' ')[1], 'Liza loves Debraj'); 
+        console.log(verifiedUser);
+        const comment = await Comment.findById(req.params.commentId)
+        console.log(comment);
+        if(comment.dislikedBy.includes(verifiedUser._id))
+        {
+            return res.status(500).json({
+                error:'You have already disliked this comment'
+            });
+        }
+
+        if(comment.likedBy.includes(verifiedUser._id))
+        {
+            comment.likes -= 1;
+            comment.likedBy = comment.likedBy.filter(userId => userId.toString() !== verifiedUser._id);
+        }
+        comment.dislike += 1;
+        comment.dislikedBy.push(verifiedUser._id);
+        await comment.save();
+        res.status(200).json({
+            message:'Comment disliked',
+            
+        });
+    }
+    catch(err)
+    {
+        console.log(err);
+        res.status(500).json({error:err});
+    }
+});
+
 
 module.exports = Router;
