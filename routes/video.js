f33517386fd5ced67697f09850e0ad660ec36af1
Debrajsingh <debrajdeb201@gmail.com>
2025-08-28 13:59:05 +0530

    views api

--- routes/video.js
+++ routes/video.js
@@ -236,4 +236,25 @@ Router.put('/dislike/:videoId',checkAuth,async(req,res)=>{
     }
 });
 
+//views api
+
+Router.put('/views/:videoId',async(req,res)=>{
+    try
+    {
+        const video = await Video.findById(req.params.videoId)
+        console.log(video);
+        video.views += 1;
+        await video.save();
+        res.status(200).json({
+            message:'Viewed',
+        });
+        
+    }
+    catch(err)
+    {
+        console.log(err);
+        res.status(500).json({error:err});
+    }
+})
+
 module.exports = Router;
